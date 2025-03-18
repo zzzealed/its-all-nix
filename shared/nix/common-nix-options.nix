9db87eea975f17b67453dc022474e5c8ce477029
Zeal <zeal@zzzealed.com>
2025-03-18 14:41:20 +0100

    user packages refactor

--- shared/nix/common-nix-options.nix
+++ shared/nix/common-nix-options.nix
@@ -42,8 +42,6 @@
       gitMinimal
       fastfetch
       wget
-      zellij
-      yazi
     ];
   };
 }
--- shared/nix/mads-user.nix
+++ shared/nix/mads-user.nix
@@ -1,28 +1,36 @@
+{ pkgs, flakeInputs, ... }:
 {
   config = {
-
     nix.settings.trusted-users = [
       "mads"
     ];
-    
-    # User(s)
+    programs.fish.enable = true;
     users.users.mads = {
       description = "Main user account";
       isNormalUser = true;
       hashedPassword = "$y$j9T$6vFvxN86DFTOIwcl.Zq6e0$gl59welPFvDnvVrAdaE4zbN8QO2Qir6rwVbrmdfPM97";
       home = "/home/mads";
-
       extraGroups = [
         "wheel" # sudo access
         "docker"
         "samba"
         "vault"
       ];
+      packages = with pkgs; [
+        zellij
+        unstable.yazi
+        bat
+        eza
+        fd
+        ripgrep
+        fzf
+        tealdeer
+        navi
+      ];
     };
-
-    # Home Manager
     home-manager.users.mads = {
       xdg.enable = true;
+      programs.fish.enable = true;
       imports = [
         ../home-manager/mpv
         ../home-manager/git
