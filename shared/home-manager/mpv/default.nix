60a7cabb25450412f76c6406cf1ea85e44d00f0b
Zeal <zeal@zzzealed.com>
2025-05-05 01:28:09 +0200

    whole buncha shieeetttt

--- shared/home-manager/mpv/default.nix
+++ shared/home-manager/mpv/default.nix
@@ -2,9 +2,6 @@
 
 {
   config = {
-    home.packages = [ pkgs.unstable.mpv ];
-
-    # Link the shared MPV configuration
     home.file.".config/mpv" = {
       source = ./config;
       recursive = true;
