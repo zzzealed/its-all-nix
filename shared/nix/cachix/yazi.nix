1fa452ca9f013b21c71e2065c839a5ca56e570d1
Zeal <zeal@zzzealed.com>
2025-05-09 12:12:17 +0200

    update flake

---
+++ shared/nix/cachix/yazi.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      extra-substituters = [
+        "https://yazi.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+      ];
+    };
+  };
+}
